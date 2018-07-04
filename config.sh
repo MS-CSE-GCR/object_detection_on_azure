761ea4636a52ac932281ced87f24b7d611accee2
Jie Lin (FA TALENT) <t-jil@microsoft.com>
2018-07-04 18:21:49 +0800

    change user

--- config.sh
+++ config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-mkdir /home/msfthack
-cd /home/msfthack
+echo "hello"
+whoami
+su - jvm<<!
+pwd
+whoami
+mkdir ~/msfthack
+cd ~/msfthack
 echo "cloning darknet"
 git clone https://github.com/Iamnvincible/darknet.git
 cd darknet
@@ -11,11 +16,17 @@ cd ..
 git clone https://github.com/fizyr/keras-retinanet.git
 cd keras-retinanet 
 pip install . --user
-cd /home/msfthack
+cd ~/msfthack
 mkdir .jupyter
 cd .jupyter
 wget https://raw.githubusercontent.com/Iamnvincible/jupyterconfig/master/jupyter_notebook_config.py
-cd /home/msfthack
+cd ~/msfthack
 echo "starting jupyter notebook on port 9999"
 jupyter notebook >jupyter.log 2>&1 &
 echo "done"
+whoami
+pwd
+exit
+!
+whoami
+echo "byebye"
